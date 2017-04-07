191a9a10a40a3300d9f112ba720e51ce452db19a
xc-racer99 <xc-racer2@live.ca>
2017-04-07 14:23:10 -0700

    Make an array of the needed data from basedata

--- shp2dxf.pl
+++ shp2dxf.pl
@@ -8,25 +8,35 @@ my $custom_isom_offset = 0;
 
 # Open the BaseNames.csv file
 my $basefile = 'csv/BaseNames.csv';
-
 open(my $basedata, '<', $basefile) or die "Couldn't find $basefile";
+
+# Create an multi-dimensional array, [i][0] = filename, [i][1] = Layer name, [i][2] = ISOM code
+my @bdarray = ();
+my $i = 0;
+while (my $line = <$basedata>) {
+	chomp $line;
+
+	my @fields = split "," , $line;
+	push @{ $bdarray[$i] }, $fields[0];
+	push @{ $bdarray[$i] }, $fields[1];
+ 	push @{ $bdarray[$i] }, $fields[$common_isom_offset];
+	$i++;
+}
+close $basedata;
 
 # Create common CRT file
 my $common_crt_file = 'Common.crt';
 open(my $common_crt, '>', $common_crt_file) or die "Couldn't open $common_crt_file";
 
-while (my $line = <$basedata>) {
-	chomp $line;
-
-	my @fields = split "," , $line;
-	if ($fields[1] eq "NAME" ) {
-		print $common_crt $fields[$common_isom_offset] . " " . $fields[0] . "\n";
+for (my $i = 0; $i < $#bdarray; $i++) {
+	if ($bdarray[$i][1] eq "NAME" ) {
+		print $common_crt $bdarray[$i][$common_isom_offset] . " " . $bdarray[$i][0] . "\n";
 	}
 }
 close $common_crt;
 
-# Loop through each file in SHP files directory
+# Loop through each of the .shp files
 
 
 
-close $basedata;
+
