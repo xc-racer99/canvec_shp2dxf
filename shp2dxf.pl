bacba7ea5cd45ef79a1e49cb7802ae009f7bde38
xc-racer99 <xc-racer2@live.ca>
2017-04-07 16:07:58 -0700

    Implement the common one
    Still need to figure out how to create a custom Layer name...

--- shp2dxf.pl
+++ shp2dxf.pl
@@ -2,9 +2,21 @@ use strict;
 use warnings;
 use 5.010;
 
+if (!defined $ARGV[1]) {
+	die "Usage: shp2dxf dirName EPSGCode\n";
+}
+
+use File::Copy qw(copy);
+
 # Setup the offsets for the ISOM codes
 my $common_isom_offset = 2;
 my $custom_isom_offset = 0;
+
+# Setup projection
+my $epsg = "EPSG:$ARGV[1]";
+
+# Create output folder
+mkdir "$ARGV[0]-DXF", 0755;
 
 # Open the BaseNames.csv file
 my $basefile = 'csv/BaseNames.csv';
@@ -36,7 +48,42 @@ for (my $i = 0; $i < $#bdarray; $i++) {
 close $common_crt;
 
 # Loop through each of the .shp files
+my @shpfiles = glob "$ARGV[0]/*.shp";
 
+foreach my $file (@shpfiles) {
+	my $dirnamelength = (length $ARGV[0]) + 1;
+	my $filename = substr $file, $dirnamelength, ((length $file) - $dirnamelength - 4);
 
+	my $ourindex = -1;
 
+	for (my $i = 0; $i < $#bdarray; $i++) {
+		if ($filename eq $bdarray[$i][0]) {
+			$ourindex = $i;
+			last;
+		}
+	}
 
+	if ( $ourindex == -1 ) {
+		warn "$filename not found in BaseData, ignorning\n";
+		next;
+	}
+
+	if ($bdarray[$ourindex][1] eq "FALSE") {
+		# Don't use this symbol
+		next;
+	} elsif ($bdarray[$ourindex][1] eq "NAME") {
+		# Convert to DXF, use the generic CRT
+		system("ogr2ogr -skipfailures -f DXF $ARGV[0]-DXF/$filename.dxf $file -sql 'SELECT map_sel_en AS Layer FROM $filename' -t_srs $epsg -dsco HEADER=header.dxf");
+		copy "Common.crt", "$ARGV[0]-DXF/$filename.crt";
+	} else {
+		# Convert to DXF, use a custom CRT
+	}
+}
+
+sub createCustomCRT {
+
+}
+
+sub getFilteredEntries {
+
+}
